50eede6b2fe8b5fbd78ff6e3c36c91bb7e716429
Ashwani123-cr <115205067+Ashwani123-cr@users.noreply.github.com>
2026-03-04 18:14:41 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,202 @@
+import HeroImage from "/assets/hero-img.webp";
+
+const Image = {
+  HeroImage,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools2 from "/assets/tools/reactjs.png";
+import Tools3 from "/assets/tools/nextjs.png";
+import Tools4 from "/assets/tools/tailwind.png";
+import Tools5 from "/assets/tools/c.png";
+import Tools6 from "/assets/tools/js.png";
+import Tools7 from "/assets/tools/nodejs.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools9 from "/assets/tools/numpy.png";
+import Tools10 from "/assets/tools/pandas.png";
+import Tools11 from "/assets/tools/powerbi.png";
+import Tools12 from "/assets/tools/Python.png";
+import Tools13 from "/assets/tools/Tableau.png";
+import Tools14 from "/assets/tools/html.png";
+import Tools15 from "/assets/tools/css.png";
+import Tools16 from "/assets/tools/r.png";
+import Tools17 from "/assets/tools/mysql.png";
+
+
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 2,
+    gambar: Tools2,
+    nama: "React JS",
+    ket: "Framework",
+    dad: "200",
+  },
+  {
+    id: 3,
+    gambar: Tools3,
+    nama: "Next JS",
+    ket: "Framework",
+    dad: "300",
+  },
+  {
+    id: 4,
+    gambar: Tools4,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "400",
+  },
+  {
+    id: 5,
+    gambar: Tools5,
+    nama: "C++",
+    ket: "Framework",
+    dad: "500",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  {
+    id: 7,
+    gambar: Tools7,
+    nama: "Node JS",
+    ket: "Javascript Runtime",
+    dad: "700",
+  },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+  {
+    id: 9,
+    gambar: Tools9,
+    nama: "Numpy",
+    ket: "library",
+    dad: "900",
+  },
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "Pandas",
+    ket: "library",
+    dad: "1000",
+  },
+  {
+    id: 11,
+    gambar: Tools11,
+    nama: "PowerBI",
+    ket: "visualization",
+    dad: "1100",
+  },
+  {
+    id: 12,
+    gambar: Tools12,
+    nama: "Python",
+    ket: "Language",
+    dad: "1200",
+  },
+  {
+    id: 13,
+    gambar: Tools13,
+    nama: "Tableau",
+    ket: "Visualization",
+    dad: "1300",
+  },
+  {
+    id: 14,
+    gambar: Tools14,
+    nama: "HTML",
+    ket: "Language",
+    dad: "1400",
+  },
+  {
+    id: 15,
+    gambar: Tools15,
+    nama: "CSS",
+    ket: "Language",
+    dad: "1500",
+  },
+  {
+    id: 16,
+    gambar: Tools16,
+    nama: "R",
+    ket: "Language",
+    dad: "1600",
+  },
+  {
+    id: 17,
+    gambar: Tools17,
+    nama: "MySQL",
+    ket: "Framework",
+    dad: "1700",
+  },
+  
+];
+
+import Proyek1 from "/assets/project/proyek1.jpg";
+import Proyek2 from "/assets/project/proyek2.jpg";
+import Proyek3 from "/assets/project/proyek3.jpg";
+import Proyek4 from "/assets/project/proyek4.jpg";
+
+export const listProyek = [
+  {
+    id: 1,
+    image: Proyek1,
+    title: "Modern E-Commerce Web Platform",
+    subtitle: "Responsive online shopping website built using HTML and CSS.",
+    fullDescription:"Developed a clean and responsive e-commerce website interface using HTML and CSS in Visual Studio Code. The project focuses on creating a user-friendly shopping experience with structured product listings, intuitive navigation, and modern UI design. It demonstrates front-end development skills including responsive layouts, styling, and organized webpage structure",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://github.com/Ashwani123-cr",
+    dad: "100",
+  },
+  {
+    id: 2,
+    image: Proyek2,
+    title: "Cyclistic Bike Share Data Analysis",
+    subtitle: "Google Data Analytics Capstone Project focused on extracting business insights.",
+    fullDescription:"Performed end-to-end data analysis using the Cyclistic bike-share dataset as part of the Google Data Analytics Professional Certificate. The project involved data cleaning, exploration, and visualization using tools such as R, Excel, and Tableau. Key insights were generated to understand customer behavior and provide data-driven recommendations for business growth.",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://github.com/Ashwani123-cr1",
+    dad: "200",
+  },
+  {
+    id: 3,
+    image: Proyek3,
+    title: "Freelancer Connect Web Platform",
+    subtitle: "A website connecting clients with skilled freelancers.",
+    fullDescription:"Designed and developed a simple freelancing platform using HTML and CSS that allows users to explore freelancer services and project opportunities. The website focuses on a structured layout, service categories, and easy navigation to simulate a real-world freelance marketplace.",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://github.com/Ashwani123-cr",
+    dad: "300",
+  },
+  {
+    id: 4,
+    image: Proyek4,
+    title: "Ocean AI – Float Chat",
+    subtitle: "AI-powered system providing ocean insights and environmental understanding...",
+    fullDescription:"Float Chat is an Ocean AI project developed as part of the Smart India Hackathon (SIH). The system focuses on analyzing ocean-related data and providing meaningful insights about ocean conditions and environmental patterns. Using structured flowcharts and AI-based concepts, the project aims to help users understand ocean behavior, water conditions, and marine insights through an easy and interactive platform.",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://github.com/Ashwani123-cr",
+    dad: "400",
+  },
+    
+];
